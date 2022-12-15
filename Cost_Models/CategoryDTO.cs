c3894b23f0589427a0ee23fb0c519cac5f047463
zen <zen@zen>
2022-12-15 11:07:26 -0800

    category

---
+++ Cost_Models/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cost_Models
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name..")]
+        public string Name { get; set; }
+    }
+}
