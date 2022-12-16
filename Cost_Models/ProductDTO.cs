ad024a30f423f49aca012bfc20facc431aae85a3
zen <zen@zen>
2022-12-16 11:23:15 -0800

    product dto

---
+++ Cost_Models/ProductDTO.cs
@@ -0,0 +1,18 @@
+﻿using Cost_DataAccess;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Cost_Models
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name..")]
+        public string Name { get; set; }
+        public double Cost { get; set; }
+        public string Other { get; set; }
+
+        [Required(ErrorMessage = "Please enter at least one ingredient..")]
+        public List<Component> Components { get; set; }
+}
