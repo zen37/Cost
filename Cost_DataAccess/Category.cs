73cec2081389358907b2a6e64b2a13d8b4c2d568
zen <zen@zen>
2022-12-16 09:27:49 -0800

    category audit

--- Cost_DataAccess/Category.cs
+++ Cost_DataAccess/Category.cs
@@ -12,5 +12,10 @@ namespace Cost_DataAccess
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedTimestamp { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedTimestamp { get; set; }
+        public bool IsActive { get; set; }
     }
 }
