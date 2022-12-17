3c14901ff22046f03c78a37724694a0ffa89e6d1
zen <zen@zen>
2022-12-17 10:45:18 -0800

    component interface

---
+++ Cost_Services/IRepository/IComponentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Cost_DataAccess;
+
+namespace Cost_Services.IRepository
+{
+    public  interface IComponentRepository
+    {
+        Task AddComponentAsync(Component component);
+        Task UpdateComponentAsync(Component component);
+        Task<Component>? GetComponentByIdAsync(int componentId);
+        Task DeleteComponentAsync(int componentId);
+        Task<IEnumerable<Component>> GetComponentsByUserIdAsync(string userId);
+
+        // Task<IEnumerable<Component>> GetComponentsByCategAsync(string user, string category);
+    }
+}
