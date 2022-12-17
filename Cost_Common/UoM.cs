c2a5f4ddc2c4ca4ac4bb54ddd1371e8adf1b5ad3
zen <zen@zen>
2022-12-16 19:01:09 -0800

    unit of measure

---
+++ Cost_Common/UoM.cs
@@ -0,0 +1,11 @@
+﻿namespace Cost_Common
+{
+    public class UoM
+    {
+        public string Name { get; set; }
+        public string Abbreviation { get; set; }
+        public string Class { get; set; }
+        public double Factor { get; set; }
+
+    }
+}
