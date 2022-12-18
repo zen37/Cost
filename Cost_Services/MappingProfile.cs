b9ecf4834dbf26d2a6048941df8df58b66342037
zen <zen@zen>
2022-12-17 16:43:22 -0800

    mapping profile data to model

---
+++ Cost_Services/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cost_DataAccess;
+using Cost_Models;
+
+
+namespace Cost_Services
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Component, ComponentDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+
+        }
+    }
+}
